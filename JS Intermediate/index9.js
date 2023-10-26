217231081347a4c8d0dc2d612f723d81f8334f9c
Ryan24Price <illusion165@hotmail.com>
2023-10-25 20:49:55 -0400

    intial setup

---
+++ JS Intermediate/index9.js
@@ -0,0 +1,17 @@
+// Question 9
+
+// 9. Given the below salaries object, perform the following tasks.
+
+let salaries = {
+    "Timothy" : 35000,
+    "David" : 25000,
+    "Mary" : 55000,
+    "Christina" : 75000,
+    "James" : 43000
+    };
+
+// a) Write a function sumSalaries(salaries) that calculates and returns the total of all
+// salaries
+// b) Write a function topEarner(salaries) that calculates and returns the name of the
+// person earning the highest salary
+
